42d7c0560d88b89e83c229797ad7d80f67f9d984
moquan4543 <moquan4543@gmail.com>
2024-01-24 17:47:05 +0800

    add instruction ROM module

---
+++ inst_rom.v
@@ -0,0 +1,20 @@
+//以byte尋址
+module inst_rom(
+    input wire ce,
+    input wire[`InstAddrBus] addr,
+    output reg[`InstBus] inst
+);
+    reg[`InstBus] inst_mem[0:`InstMemNum-1];
+
+    //初始化指令存儲器
+    initial $readmemh ( "inst_rom.data", inst_mem);
+
+    //根據輸入的位址，輸出元素
+    always @ (*) begin
+        if(ce == `ChipDisable) begin
+            inst <= `ZeroWord;
+        end else begin
+            inst <= inst_mem[addr[`InstMemNumLog2+1:2]];
+        end
+    end
+endmodule
