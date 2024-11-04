9eba645f877b49f26bd72fe838b1cec4c369467c
moquan4543 <acs111104@gm.ntcu.edu.tw>
2024-11-05 00:15:50 +0800

    implement commands

--- ex.v
+++ ex.v
@@ -17,8 +17,9 @@ module ex(
 );
     //儲存運算結果
     reg[`RegBus] logicout;
+    reg[`RegBus] shiftres;
 
-    //Step.1 根據aluop_i指定的子運算類型進行運算
+    //根據aluop_i指定的子運算類型進行運算
     always @ (*) begin
         if(rst == `RstEnable) begin
             logicout <= `ZeroWord;
@@ -27,20 +28,53 @@ module ex(
             `EXE_OR_OP: begin
                 logicout <= reg1_i | reg2_i;
             end
+            `EXE_AND_OP: begin
+                logicout <= reg1_i & reg2_i;
+            end
+            `EXE_NOR_OP: begin
+                logicout <= ~ (reg1_i | reg2_i);
+            end
+            `EXE_XOR_OP: begin
+                logicout <= reg1_i ^ reg2_i;
+            end
             default:begin
                 logicout <= `ZeroWord;
             end
             endcase
         end
     end
+    //位移運算
+    always @ (*) begin
+        if(rst == `RstEnable) begin
+            shiftres <= `ZeroWord;
+        end else begin
+            case(aluop_i)
+            `EXE_SLL_OP: begin //邏輯左移
+                shiftres <= reg2_i << reg1_i[4:0];
+            end
+            `EXE_SRL_OP: begin //邏輯右移
+                shiftres <= reg2_i >> reg1_i[4:0];
+            end
+            `EXE_SRA_OP: begin //算數右移
+                shiftres <= ({32{reg2_i[31]}}<<(6'd32-{1'b0, reg1_i[4:0]})) | reg2_i >> reg1_i[4:0];
+            end
+            default: begin
+                shiftres <= `ZeroWord;
+            end
+            endcase
+        end
+    end
 
-    //Step.2 根據alusel_i指定的運算類型，選擇運算結果
+    //根據alusel_i指定的運算類型，選擇運算結果
     always @ (*) begin
         wd_o <= wd_i;
         wreg_o <= wreg_i;
         case (alusel_i)
         `EXE_RES_LOGIC: begin
             wdata_o <= logicout;
+        end
+        `EXE_RES_SHIFT: begin
+            wdata_o <= shiftres;
         end
         default:begin
             wdata_o <= `ZeroWord;
